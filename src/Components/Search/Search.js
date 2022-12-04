df9dc1e228a11c6b435ef818629b351b48d83928
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-12-04 18:41:19 +0330

    add search button icon

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,8 +1,10 @@
-
+import '../../font-awesome/css/font-awesome.min.css';
+import Button from "../UI/Button";
 const Search = ({placeholder}) => {
     return (
         <form>
             <input placeholder={placeholder}/>
+            <Button><i className="fa fa-search" aria-hidden="true"></i></Button>
         </form>
     );
 };
