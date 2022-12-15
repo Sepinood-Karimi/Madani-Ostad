de9055e456474ab001373d14cdd7233f27f1627d
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-12-15 15:55:36 +0330

    add navbar styles

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -1,16 +1,19 @@
 import Button from "../UI/Button";
 import Search from "../Search/Search";
+import classes from './NavBar.module.css';
 
 const NavBar = () => {
     return (
-        <header>
+        <header className={classes.header}>
             <div>
-                <h2>LOGO</h2>
-                <h3>مدنی استاد </h3>
-                <p>!این بار تو به اساتید نمره بده</p>
+                <h3 className={classes.logo}>مدنی استاد </h3>
+                <span className={classes['main-title']}>
+                    <h2>LOGO</h2>
+                    <p>!این بار تو به اساتید نمره بده</p>
+                </span>
             </div>
             <Search placeholder="دنبال کدوم استاد می گردی ؟"/>
-            <nav>
+            <nav className={classes.navbar}>
                 <Button>ورود <i className="fa fa-user" aria-hidden="true"></i></Button>
                 <a> دانشکده ها </a>
                 <a> تماس با ما </a>
