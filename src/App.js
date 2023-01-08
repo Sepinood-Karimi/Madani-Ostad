7e64b8303ca03f9489bf22a3d9c871c5a14b0fb9
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-01-08 20:51:31 +0330

    add main component and edit navbar styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from "./Components/Header/NavBar";
+import Main from "./Components/MainPage/Main";
 
 function App() {
     return (
         <>
             <NavBar/>
+            <Main/>
         </>
     );
 }
