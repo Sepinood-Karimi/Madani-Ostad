d65802176fdb319a06deaeb928a8645f9ab46ec7
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-01-17 14:48:05 +0330

    create faculties component and style the title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from "./Components/Header/NavBar";
 import Main from "./Components/MainPage/Main/Main";
+import Faculties from "./Components/MainPage/Faculties/Faculties";
 
 function App() {
     return (
         <>
             <NavBar/>
             <Main/>
+            <Faculties/>
         </>
     );
 }
---
+++ src/Components/MainPage/Faculties/Faculties.js
@@ -0,0 +1,24 @@
+import classes from './Faculties.module.css';
+const Faculties = () => {
+  return(
+      <section>
+          <div className={classes['faculties-title']}>
+              <div className={classes['faculties-icon']}>
+                  <i className="fa fa-university" aria-hidden="true"></i>
+              </div>
+              <div>
+                  <div className={classes['faculties-main-titles']}>
+                      <h3 className={classes['faculties-main-title']}>دانشکده ها</h3>
+                      <h4 className={classes['faculties-suggestion']}> پیشنهاد دانشکده جدید</h4>
+                  </div>
+                  <p className={classes['faculties-explanation']}> لیست دانشکده ها و اساتید رو میتونی این جا ببینی</p>
+              </div>
+          </div>
+          <div>
+
+          </div>
+      </section>
+  );
+};
+
+export default Faculties;
