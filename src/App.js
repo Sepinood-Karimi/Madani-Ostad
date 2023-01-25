e27fff41af49a3f354d652b30120eaace18f8a44
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-01-25 13:32:15 +0330

    fix modal styles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ import Main from "./Components/MainPage/Main/Main";
 import Faculties from "./Components/MainPage/Faculties/Faculties";
 import ContactUs from "./Components/MainPage/Contact-us/ContactUs";
 import "../src/font-awesome/css/font-awesome.min.css";
-// import AllTeachers from "./Components/MainPage/Faculties/Faculty/Teachers/AllTeachers/AllTeachers";
-// import NewTeacher from "./Components/MainPage/Faculties/New/NewTeacher";
-// import NewFaculty from "./Components/MainPage/Faculties/New/NewFaculty";
-// import SignUp from "./Components/SignUp/SignUp";
 
 function App() {
   return (
     <>
-      {/*<SignUp />*/}
-      {/*<NewFaculty />*/}
-      {/*<NewTeacher />*/}
-      {/*<AllTeachers />*/}
       <NavBar />
       <Main />
       <Faculties />
--- src/Components/MainPage/Faculties/Faculty/Teachers/AllTeachers/AllTeachers.js
+++ src/Components/MainPage/Faculties/Faculty/Teachers/AllTeachers/AllTeachers.js
@@ -1,58 +1,113 @@
-import classes from "../../Faculty.module.css";
 import mainClasses from "../../../../../UI/Common/common.module.css";
 import Modal from "../../../../../UI/Modal/Modal";
-import allTeachersClasses from "./AllTeachers.module.css";
+import classes from "./AllTeachers.module.css";
 import classnames from "classnames";
 
 const AllTeachers = ({ isOpen, close, openNewTeacherModal }) => {
   return (
     <Modal
-      additionalModalClasses={classnames(
-        allTeachersClasses["all-teachers__modal"]
-      )}
+      additionalModalClasses={classnames(classes["all-teachers__modal"])}
       isOpen={isOpen}
       closeModal={close}
     >
       <div>
-        <p className={classes["faculty-title"]}>دانشکده</p>
-        <h4 className={classes["faculty-name"]}> فناوری اطلاعات</h4>
-        <ul className={classes.teachers}>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+        <p className={classnames(classes["title"])}>دانشکده</p>
+        <h4 className={classnames(classes["faculty-name"])}> فناوری اطلاعات</h4>
+        <ul className={classnames(classes["teachers"])}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
-          <li className={classes.teacher + " " + mainClasses["hand-cursor"]}>
+          <li
+            className={classnames(
+              mainClasses["hand-cursor"],
+              classes["teacher"]
+            )}
+          >
             مهدی هاشم زاده
           </li>
         </ul>
         <p
-          className={classes["all-teachers"] + " " + mainClasses["hand-cursor"]}
+          className={classnames(
+            mainClasses["hand-cursor"],
+            classes["new-teacher"]
+          )}
           onClick={openNewTeacherModal}
         >
           پیشنهاد استاد جدید
