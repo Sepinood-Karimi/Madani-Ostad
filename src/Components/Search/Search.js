7d018a0ce4ec8f7b33637776053bb02ef3054457
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-02-04 14:32:00 +0330

    search list implementation

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,24 +1,39 @@
 import Button from "../UI/Button/Button/Button";
 import classes from "./Search.module.css";
-import { useEffect } from "react";
+import { useRef, useState } from "react";
 
 const Search = ({ placeholder }) => {
+  const [teachersList, setTeachersList] = useState([]);
+  const teacherInputRef = useRef();
   const getTeachers = async () => {
     const response = await fetch(
       "https://api.kodoomostad.rezakargar.ir/api/v1/Professors"
     );
     const data = await response.json();
-    console.log(data);
+    return data.data;
   };
-  useEffect(() => {
-    getTeachers().then();
-  }, []);
+
+  const getTeachersList = () => {
+    getTeachers().then((teachers) => setTeachersList(teachers));
+  };
+
   return (
     <form>
       <Button className={classes["search-button"]}>
         <i className="fa fa-search" aria-hidden="true"></i>
       </Button>
-      <input className={classes["search-input"]} placeholder={placeholder} />
+      <input
+        className={classes["search-input"]}
+        placeholder={placeholder}
+        list="teachers"
+        onClick={getTeachersList}
+        ref={teacherInputRef}
+      />
+      <datalist id="teachers">
+        {teachersList.map((teacher) => (
+          <option key={teacher.id}>{teacher.name}</option>
+        ))}
+      </datalist>
     </form>
   );
 };
